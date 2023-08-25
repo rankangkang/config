efba03e3ca8bb771ef9b3d34efdf60a0f74b5793
cmKangkang <cmkangkang@foxmail.com>
2023-08-25 19:08:40 +0800

    feat: update

--- packages/eslint-config-react/index.js
+++ packages/eslint-config-react/index.js
@@ -2,7 +2,7 @@ module.exports = {
   extends: ['plugin:react/recommended', 'plugin:react-hooks/recommended', '@cmkk/eslint-config-ts'],
   settings: {
     react: {
-      version: '>=17',
+      version: '18.20.0',
     },
   },
   rules: {
--- packages/eslint-config-ts/index.js
+++ packages/eslint-config-ts/index.js
@@ -3,22 +3,34 @@ module.exports = {
     node: true,
     es2022: true,
   },
-  extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier',
+    'plugin:prettier/recommended',
+  ],
   overrides: [],
   parser: '@typescript-eslint/parser',
   parserOptions: {
     ecmaVersion: 'latest',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint', 'prettier'],
+  plugins: ['prettier'],
   rules: {
-    'prettier/prettier': 2,
+    'prettier/prettier': [
+      'error',
+      {},
+      {
+        usePrettierrc: true,
+      },
+    ], // 开启 prettier 规则
     'prefer-const': 0,
     'space-before-function-paren': 'off',
     'no-undef': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
     'no-unused-vars': ['off', { argsIgnorePattern: '^_' }],
     '@typescript-eslint/no-unused-vars': ['off', { argsIgnorePattern: '^_' }],
+    '@typescript-eslint/no-non-null-assertion': 'off',
     'comma-dangle': [
       'error',
       {
